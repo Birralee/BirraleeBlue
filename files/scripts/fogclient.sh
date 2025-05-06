da0ed2e31129fc7c6dd07ccc991cd4a39d0fca50
birralee-ict <162308039+birralee-ict@users.noreply.github.com>
2025-05-06 12:44:39 +0200

    Revert "feat: add fogclient to chrome laptops"
    This reverts commit 1c03ca28b403bb28cfb072afb4bff02f8a000926.

--- files/scripts/fogclient.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Grab the latest fogclient and install it
-curl -Lo /tmp/SmartInstaller.exe $(curl https://api.github.com/repos/FOGProject/fog-client/releases/latest | jq -r '.assets[] | select(.name| test("SmartInstaller.exe$")).browser_download_url')
-mono /tmp/SmartInstaller.exe --server="fog.birralee.local"
