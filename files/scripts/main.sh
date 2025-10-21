9af162461c9fa336cf7da7f943631fe52e800bc1
birralee-ict <162308039+birralee-ict@users.noreply.github.com>
2025-10-21 10:17:15 +0200

    feat(main): add IT software

--- files/scripts/main.sh
+++ files/scripts/main.sh
@@ -32,5 +32,17 @@ rpm-ostree install \
     tuned-profiles-atomic \
     tuned-profiles-cpu-partitioning
 
+# Adding repositories should be a LAST RESORT. Contributing to Terra or `ublue-os/packages` is much preferred
+# over using random coprs. Please keep this in mind when adding external dependencies.
+# If adding any dependency, make sure to always have it disabled by default and _only_ enable it on `dnf install`
+
+dnf5 config-manager addrepo --set=baseurl="https://packages.microsoft.com/yumrepos/vscode" --id="vscode"
+dnf5 config-manager setopt vscode.enabled=0
+# FIXME: gpgcheck is broken for vscode due to it using `asc` for checking
+# seems to be broken on newer rpm security policies.
+dnf5 config-manager setopt vscode.gpgcheck=0
+dnf5 install --nogpgcheck --enable-repo="vscode" -y \
+    code
+
 # Enable student user generation
 systemctl enable student-user.service
